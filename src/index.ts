325cb82899172087db6af3e3e94977a6a82af1c5
fipnooone <fipnone@gmail.com>
2023-06-06 18:24:35 +0700

    modal controls type exported

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 import { Dialog, useModal, useModalContext } from './modules';
 
+export type { ModalControls } from './modules';
+
 export default useModal;
 export { Dialog, useModalContext };
--- src/modules/index.tsx
+++ src/modules/index.tsx
@@ -3,3 +3,4 @@ export { useModalContext } from './context';
 export { Dialog } from './dialog';
 export { events } from './events';
 export { useModal } from './hook';
+export type { ModalControls } from './hook/types';
